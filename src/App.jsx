1d63da3c55896bee5ddd876d82a1f1d33c70466b
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-22 14:02:08 -0300

    :lipstick: reset css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import Home from './pages/home/Home'
- 
-
-
+import { useState } from "react";
+import { Home } from "./pages/home/Home";
+import "./css/reset.css";
 function App() {
-
   return (
     <div className="App">
- <Home/>
+      <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
