b308e467f45245746c7def92b9037c2ecfbf22b3
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-23 17:38:37 -0300

    :sparkles: feat:  finishing the page login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,25 +2,39 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Button } from "../../components/Button";
 import { Headers } from "../../components/Headers";
+import { AuthContext } from "../../providers/Auth";
+import { postSignin } from "../../servers/UserServices";
 import { Logo } from "../logo/Logo";
 import { Containner } from "../register/Register";
 
 export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const { token, setToken } = React.useContext(AuthContext);
   const navigate = useNavigate();
-  function handleForm(e) {
-    e.preventDefault();
-    navigate("/");
-  }
-  const register = {
+  const signin = {
     email: email,
     password: password,
   };
-  console.log(register);
+
+  function handleForm(e) {
+    e.preventDefault();
+
+    const user = postSignin(signin);
+    user.then((response) => {
+        const { token } = response.data;
+        setToken(token);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
     <>
-      <Headers cadastrar={"cadastrar"} entrar={"entrar"} link={'entrar'} />
+      <Headers cadastrar={"cadastrar"} entrar={"entrar"} link={"entrar"} />
       <Logo />
       <Containner onSubmit={handleForm}>
         <input
@@ -35,7 +49,7 @@ export const Login = () => {
           required
           onChange={(e) => setPassword(e.target.value)}
         />
-        <Button text={'Entrar'} type="submit" />
+        <Button text={"Entrar"} type="submit" />
       </Containner>
     </>
   );
