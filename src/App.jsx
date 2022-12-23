4ef95c992ea1ac451ae795147c382097f7e20f5c
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-22 21:19:53 -0300

    :sparkles: feat: all routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
 import { Home } from "./pages/home/Home";
 import "./css/reset.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Register } from "./pages/register/Register";
+import { Login } from "./pages/login/Login";
+import { Links } from "./pages/links/Links";
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/link" element={<Links />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
