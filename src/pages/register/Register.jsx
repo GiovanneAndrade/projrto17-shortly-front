ff3c5319e944154b228174301b9fda9d954d0fda
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-23 17:37:45 -0300

    :sparkles: feat:  finishing the page register

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,39 +1,64 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from "react-router-dom";
 import { Headers } from "../../components/Headers";
 import { Logo } from "../logo/Logo";
 import styled from "styled-components";
 import { Button } from "../../components/Button";
-export const Register = ({type}) => {
-  const [nome, setNome] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
-  const navigate = useNavigate()
-  function handleForm (e){
-    e.preventDefault();
-    navigate("/")
-  }
-  const register = {
-    nome: nome,
+import { postSignup } from "../../servers/UserServices";
+export const Register = ({ type }) => {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const navigate = useNavigate();
+  const signup = {
+    name: nome,
     email: email,
     password: password,
-    confirmPassword:confirmPassword
+    confirmPassword: confirmPassword,
+  };
+  function handleForm(e ) {
+    e.preventDefault();
+    const register = postSignup(signup);
+    register.then((response) => {
+      navigate("/");
+    }).catch((error) => {
+      console.log(error);
+    });
   }
  
-  return (
 
+  return (
     <>
-      <Headers cadastrar={"cadastrar"} entrar={"entrar"} link={'link'}/>
+      <Headers cadastrar={"cadastrar"} entrar={"entrar"} link={"link"} />
       <Logo />
-        <Containner  onSubmit={handleForm}>
-          <input type="text"  placeholder="nome" required onChange={(e)=> setNome(e.target.value)} />
-          <input type="email" placeholder="email" required onChange={(e)=> setEmail(e.target.value)}/>
-          <input type="password" placeholder="senha" required onChange={(e)=> setPassword(e.target.value)}/>
-          <input type="password" placeholder="corfirme senha" required onChange={(e)=> setConfirmPassword(e.target.value)}/>
-          <Button text={'Criar Conta'} type='submit'/>
-        </Containner>
-      
+      <Containner onSubmit={handleForm}>
+        <input
+          type="text"
+          placeholder="nome"
+          required
+          onChange={(e) => setNome(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="senha"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="corfirme senha"
+          required
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <Button text={"Criar Conta"} type="submit" />
+      </Containner>
     </>
   );
 };
@@ -54,7 +79,6 @@ export const Containner = styled.form`
     border: 1px solid rgba(120, 177, 89, 0.25);
     box-shadow: 0px 4px 24px rgba(120, 177, 89, 0.12);
     border-radius: 12px;
-   
   }
   ${(props) => {
     if (props.type === true) {
